1e98e68191f267ab3c4b0f300aba7d9a53aecd95
Francisco Nicosia <nicosia.francisco@gmail.com>
2023-09-11 12:10:08 +0200

    adding sql

---
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace Discount.API.Entities
+{
+	public class Coupon
+	{
+		public int Id { get; set; }
+		public required string ProductName { get; set; }
+		public string Description { get; set; } = string.Empty;
+		public int Amount { get; set; }
+	}
+}
