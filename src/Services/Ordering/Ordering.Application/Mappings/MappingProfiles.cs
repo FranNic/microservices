d6fcb2e6f3f962bcda201b60f6d990f1d246b3ff
Francisco Nicosia <nicosia.francisco@gmail.com>
2023-09-20 16:57:01 +0200

    adding ordering with grpc

---
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfiles.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Application.Mappings
+{
+	using AutoMapper;
+
+	using Ordering.Application.Features.Orders.Commands;
+	using Ordering.Application.Features.Orders.Queries;
+	using Ordering.Domain.Entities;
+
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<Order, OrdersVm>().ReverseMap();
+			CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+			CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+		}
+	}
+}
